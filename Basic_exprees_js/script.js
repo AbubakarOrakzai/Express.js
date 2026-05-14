042347de9629e7196684c89b3477c7a63bf1bc72
Abubakar Orakzai <abubakarorakzai15@gamail.com>
2026-05-15 00:50:26 +0500

    first commit

---
+++ Basic_exprees_js/script.js
@@ -0,0 +1,27 @@
+import express from 'express'
+
+const app = express()
+
+
+//  ====== Midleware ======
+
+app.use((req,res,next) => {
+    console.log('This is a middleware function.')
+    next()
+})
+app.get('/', (req, res) => {
+  res.send('It me Abubakar Orakzai from Orakzai Agency how are you doing?')
+})
+
+
+//  what ever the user in the brower req for will be store
+//  in the req so that why we write req.paramss.username
+//  inorder to display the username on the page.
+
+app.get('/profile/:username', (req, res) => {
+  res.send(`Hi This is ${req.params.username}`)
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
---
+++ Readme.md
@@ -0,0 +1,7 @@
+## This repo will contain all the concept related to Express.js
+
+# Basic_express_js
+
+-- This folder contain concepts
+-- How to Create Middleware and how to use it 
+-- Dynamic routing
