7bf6577ffd22ee264babc157dcc47bf237afef05
Abubakar Orakzai <abubakarorakzai15@gamail.com>
2026-05-16 19:45:36 +0500

    Mini Project added
    Signed-off-by: Abubakar Orakzai <abubakarorakzai15@gamail.com>

---
+++ Mini_Project/script.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+const path = require('path')
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')))
+app.set('view engine','ejs')    
+
+app.use((req,res,next)=>{
+    console.log('This is a middleware function.')
+    next()
+})
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+}); 
