0056cf6e96656dc78b247e5e061059811be73562
Abubakar Orakzai <abubakarorakzai15@gamail.com>
2026-05-28 02:45:20 +0500

    Mongo_DB
    Signed-off-by: Abubakar Orakzai <abubakarorakzai15@gamail.com>

--- Mongo_DB/script.js
+++ Mongo_DB/script.js
@@ -3,9 +3,21 @@ const app = express()
 
 app.get('/',(req,res)=>{
        console.log("hey")
-       res.send("hellow World")
 })
 
+app.get('/create',async(req,res)=>{
+      let createduser = await userModel.create({
+        name:"Abubakar Orakzai",
+        email:"abubakarorakzai15@gmail.com",
+        username:"Abubakar Orakzai"
+      })
+        res.send(createduser)
+})
+
+app.delete('/delete',async(req,res)=>{
+    let user = await userModel.findOneAndDelete({username:"Abubakar Orakzai"})
+      res.send(users)
+})
 app.listen(3000,()=>{
        console.log("Your server is runing on the port 3000")
 })
---
+++ Mongo_DB/usermodel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/myDatabase')
+
+const userSchema = mongoose.Schema({
+      name:string,
+      username:string,
+      email:string
+})
+
+module.exports=mongoose.model("user",userSchema)
