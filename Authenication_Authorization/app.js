2b00a34470d5ed8c72510698315cbcd38703e7e5
Abubakar Orakzai <abubakarorakzai15@gamail.com>
2026-06-03 00:29:17 +0500

    complete authentication system with login, logout, and protected routes
    Signed-off-by: Abubakar Orakzai <abubakarorakzai15@gamail.com>

--- Authenication_Authorization/app.js
+++ Authenication_Authorization/app.js
@@ -1,48 +1,95 @@
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const path = require('path')
-const app = express()
-const UserModel = require("./models/user")
-const bcrypt = require('bcrypt')  
-const jwt = require('jsonwebtoken')  
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-app.set('view engine', 'ejs')
-app.use(express.json())
-app.use(express.urlencoded({extended: true}));
+const app = express();
+
+const UserModel = require("./models/user");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
     res.render("index");
 });
 
-app.post('/create', (req, res) => {
-    let { username, email, password, age } = req.body
-    
-    bcrypt.genSalt(10, (err, salt) => {  
-        if (err) return res.status(500).send(err.message);
-        
-        bcrypt.hash(password, salt, async (err, hash) => {  
-            if (err) return res.status(500).send(err.message);
-            
-            try {
-                let createdUser = await UserModel.create({
-                    username,
-                    email,
-                    password: hash,  
-                    age
-                })
-                
-                let token = jwt.sign({ email: email }, "12345678")  
-                res.cookie("token", token)
-                res.send(createdUser)
-            } catch (error) {
-                res.status(500).send(error.message)
-            }
-        })
-    })
+app.get('/login', function (req, res) {
+    res.render("login");
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+app.post('/login', async function (req, res) {
+    let user = await UserModel.findOne({
+        email: req.body.email
+    });
+
+    if (!user) {
+        return res.send("User not found");
+    }
+
+    bcrypt.compare(req.body.password, user.password, function (err, result) {
+        if (result) {
+            let token = jwt.sign(
+                { email: user.email },
+                "shhhhhhhhhhh"
+            );
+
+            res.cookie("token", token);
+            res.redirect("/home");
+        } else {
+            res.send("Wrong password");
+        }
+    });
+});
+
+app.post('/create', function (req, res) {
+    let { username, email, password, age } = req.body;
+
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(password, salt, async function (err, hash) {
+            let createdUser = await UserModel.create({
+                username,
+                email,
+                password: hash,
+                age
+            });
+
+            let token = jwt.sign(
+                { email: email },
+                "shhhhhhhhhhh"
+            );
+
+            res.cookie("token", token);
+            res.redirect("/home");
+        });
+    });
+});
+
+app.get('/home', function (req, res) {
+    let token = req.cookies.token;
+
+    if (!token) {
+        return res.redirect("/login");
+    }
+
+    jwt.verify(token, "shhhhhhhhhhh", function (err, decoded) {
+        if (err) {
+            return res.send("Invalid Token");
+        }
+        res.render("home");
+    });
+});
+
+app.get('/logout', function (req, res) {
+    res.clearCookie("token");
+    res.redirect("/");
+});
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000");
 });
